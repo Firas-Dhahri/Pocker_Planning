158db9e7dd442781eaf8d2de30763e9e03244622
Hana1131 <mejdoub.hana18@gmail.com>
2024-03-05 22:39:09 +0100

    avatar color

--- Pocker_Front/src/app/components/message/message.component.ts
+++ Pocker_Front/src/app/components/message/message.component.ts
@@ -28,6 +28,26 @@ export class MessageComponent {
   wsClient: any;
   connected!: boolean;
 
+  colors = [
+    'aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond',
+    'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue',
+    'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkgrey',
+    'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen',
+    'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue',
+    'dimgray', 'dimgrey', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite',
+    'gold', 'goldenrod', 'gray', 'green', 'greenyellow', 'grey', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory',
+    'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow',
+    'lightgray', 'lightgreen', 'lightgrey', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightslategrey',
+    'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid',
+    'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue',
+    'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered',
+    'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink',
+    'plum', 'powderblue', 'purple', 'rebeccapurple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown',
+    'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen',
+    'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow',
+    'yellowgreen'
+  ];
+
   constructor(private messageService: MessageService) {
   }
 
@@ -107,8 +127,17 @@ export class MessageComponent {
         this.sent[index] = updatedMessage; // Remplacer le message dans la liste sent par le message mis à jour
       }
       this.wsClient.send(this.updateMessage, {}, JSON.stringify(updatedMessage));
-      this.messageService.add({severity: 'succes', summary: 'Message updated', detail: 'Message updated successfully.'});
+      this.messageService.add({severity: 'info', summary: 'Message updated', detail: 'Message updated successfully.'});
     }
+  }
+
+  getAvatarColor(userId: number): string {
+    let hash = 0;
+    for (let i = 0; i < userId.toString().length; i++) {
+      hash = 31 * hash + userId.toString().charCodeAt(i);
+    }
+    const index = Math.abs(hash % this.colors.length);
+    return this.colors[index];
   }
 
 }
