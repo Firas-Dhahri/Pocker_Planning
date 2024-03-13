6d4838782720028a0e9f8f75ffa857d232e40adc
Hana1131 <mejdoub.hana18@gmail.com>
2024-03-13 03:19:39 +0100

    updated version

--- Pocker_Front/src/app/services/messageservice.service.ts
+++ Pocker_Front/src/app/services/messageservice.service.ts
@@ -8,15 +8,18 @@ declare var webkitSpeechRecognition: any;
 export class MessageserviceService {
 
   recognition = new webkitSpeechRecognition();
+
   isStoppedSpeechRecog = false;
-  public text= '';
-  tempWords: any;
+  public text = '';
+  tempWords: string = '';
 
-  constructor() { }
+  currentLanguages: string[] = ['en-US', /*'fr-FR'*/];
 
-  init(){
+  constructor() {}
+
+  init() {
     this.recognition.interimResults = true;
-    this.recognition.lang = 'en-US';
+    this.recognition.lang = this.currentLanguages;
 
     this.recognition.addEventListener('result', (e:any) => {
       const transcript = Array.from(e.results)
@@ -29,32 +32,37 @@ export class MessageserviceService {
   }
 
   startService(){
+    this.text = '';
+
     this.isStoppedSpeechRecog = false;
     this.recognition.start();
     console.log("Speech recognition started")
-    this.recognition.addEventListener('end', (condition:any) => {
-      if (this.isStoppedSpeechRecog) {
-        this.recognition.stop();
-        console.log("End speech recognition")
-      } else {
-        this.wordConcat()
-        this.recognition.start();
-      }
-    });
+    this.recognition.addEventListener('end', this.handleRecognitionEnd);
   }
 
   stopService() {
     this.isStoppedSpeechRecog = true;
     this.wordConcat();
+    this.recognition.removeEventListener('end', this.handleRecognitionEnd); // Remove the 'end' event listener
     this.recognition.stop();
     console.log("End speech recognition")
   }
+
+  private handleRecognitionEnd = (condition: any) => {
+    if (this.isStoppedSpeechRecog) {
+      this.recognition.stop();
+      console.log("End speech recognition")
+    } else {
+      this.wordConcat()
+      this.recognition.start();
+    }
+  };
 
   wordConcat(){
     if (this.text.trim() !== '') {
       this.text = this.text + ', ' + this.tempWords;
     } else {
-      this.text = this.tempWords;
+      this.text = this.tempWords.charAt(0).toUpperCase() + this.tempWords.slice(1);
     }
     this.tempWords = '';
   }
